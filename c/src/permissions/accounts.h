d732abfc72ee91d8f56d3e10ac8b988860e349fd
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2023-12-26 21:57:49 +0000

    remove ref from account permissions

--- c/src/permissions/accounts.h
+++ c/src/permissions/accounts.h
@@ -22,7 +22,7 @@ void create_database_account(char* username, char* password, DatabasePermissions
 void delete_database_account(DatabaseAccount* account);
 void update_database_account(DatabaseAccount* account, DatabaseAccount new_account);
 
-void set_table_account_permissions(ActiveTable* table, DatabaseAccount* account, TablePermissions& permissions);
+void set_table_account_permissions(ActiveTable* table, DatabaseAccount* account, TablePermissions permissions);
 void delete_table_account_permissions(ActiveTable* table, DatabaseAccount* account);
 
 const TablePermissions* get_table_permissions_for_account(ActiveTable* table, DatabaseAccount* account, bool include_table_admin = true);
