4e16785161e6d3669247aec12ef722ff9c754cc1
Jacek <jacek@speedydev.org>
2025-04-12 18:29:16 +0100

    make valid string check stricter

--- c/src/misc/valid_string.cpp
+++ c/src/misc/valid_string.cpp
@@ -2,9 +2,11 @@
 #include <algorithm>
 
 bool misc::name_string_legal(std::string& name) {
-    // Iterate over string and check if each character is an alpha, digit or dash/underscore character.
+    if (name.length() > 32 || name.length() < 2) return false;
+
+    // Iterate over string and check if each character is an alpha, digit, lowercase or dash/underscore character.
     bool valid = std::all_of(name.begin(), name.end(), [](char c) {
-        return std::isalpha(c) || std::isdigit(c) || c == '-' || c == '_';
+        return (std::isalpha(c) && std::islower(c)) || std::isdigit(c) || c == '-' || c == '_';
     });
 
     return valid;
