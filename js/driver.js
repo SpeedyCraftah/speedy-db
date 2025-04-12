a9acf79b39feddf9f62507538606ff2667ed1f53
Jacek <jacek@speedydev.org>
2025-04-12 18:50:48 +0100

    update versions

--- js/driver.js
+++ js/driver.js
@@ -279,7 +279,7 @@ module.exports = class SpeedDBClient extends EventEmitter {
 
                 // Send handshake.
                 let handshakeData = {
-                    version: { major: 3, minor: 0 },
+                    version: { major: 3, minor: 2 },
                     options: { short_attributes: false, error_text: true }
                 };
 
