4dbba529a0ff247c079b11f6952d0bf710c065e8
Jacek <jacek@speedydev.org>
2025-04-12 18:29:05 +0100

    add file exists function

--- c/src/misc/files.cpp
+++ c/src/misc/files.cpp
@@ -6,3 +6,8 @@ bool folder_exists(const char* path) {
     struct stat sb;
     return stat(path, &sb) == 0 && S_ISDIR(sb.st_mode);
 }
+
+bool file_exists(const char* path) {
+    struct stat sb;
+    return stat(path, &sb) == 0 && S_ISREG(sb.st_mode);
+}
