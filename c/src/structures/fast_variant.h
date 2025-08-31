e5964d8f5812e0cca696d9cb2a31dc650bf62213
Jacek <jacek@speedydev.org>
2025-08-31 14:52:03 +0100

    add namespace for custom variant

--- c/src/structures/fast_variant.h
+++ c/src/structures/fast_variant.h
@@ -14,82 +14,84 @@ Unions do not handle memory management such as constructors and destructors auto
 This version contains debug checks for undefined behaviour and performs a safety check on each access, which are eliminated once the code is compiled with optimizations.
 */
 
-template <typename... Types>
-class fast_variant {
-  public:
-    fast_variant() {}
-
-    template <typename T>
-    inline void set_as() {
+namespace speedystd {
+  template <typename... Types>
+  class fast_variant {
+    public:
+      fast_variant() {}
+  
+      template <typename T>
+      inline void set_as() {
+        #if !defined(__OPTIMIZE__)
+          if (debug_instantiated) {
+            throw std::runtime_error("Debug build check: calling code tried to set the variant type twice!");
+          }
+  
+          debug_instantiated = true;
+        #endif
+  
+        new (buffer) T();
+        selected_type = get_selector_for_type<T>();
+      }
+  
+      template <typename T>
+      inline T& get() {
+        #if !defined(__OPTIMIZE__)
+          if (selected_type != get_selector_for_type<T>()) {
+            throw std::runtime_error("Debug build check: calling code tried to get variant not in use!");
+          }
+        #endif
+  
+        return *reinterpret_cast<T*>(buffer);
+      }
+  
+      ~fast_variant() {
+        if (selected_type == 0) return;
+        destroy_object_impl<Types...>();
+      }
+  
+    private:
+      static constexpr size_t size = std::max({sizeof(Types)...});
+      uint selected_type = 0;
+      char buffer[size];
+  
       #if !defined(__OPTIMIZE__)
-        if (debug_instantiated) {
-          throw std::runtime_error("Debug build check: calling code tried to set the variant type twice!");
-        }
-
-        debug_instantiated = true;
+        bool debug_instantiated = false;
       #endif
-
-      new (buffer) T();
-      selected_type = get_selector_for_type<T>();
-    }
-
-    template <typename T>
-    inline T& get() {
-      #if !defined(__OPTIMIZE__)
-        if (selected_type != get_selector_for_type<T>()) {
-          throw std::runtime_error("Debug build check: calling code tried to get variant not in use!");
+  
+      template <typename T>
+      constexpr inline uint get_selector_for_type() {
+        return get_selector_for_type_impl<T, Types...>();
+      }
+  
+      template <typename T, typename First, typename... Rest>
+      constexpr inline uint get_selector_for_type_impl() {
+        if constexpr (std::is_same_v<T, First>) {
+          return sizeof...(Types) - sizeof...(Rest);
+        } 
+        
+        else if constexpr (sizeof...(Rest) > 0) {
+          return get_selector_for_type_impl<T, Rest...>();
         }
-      #endif
-
-      return *reinterpret_cast<T*>(buffer);
-    }
-
-    ~fast_variant() {
-      if (selected_type == 0) return;
-      destroy_object_impl<Types...>();
-    }
-
-  private:
-    static constexpr size_t size = std::max({sizeof(Types)...});
-    uint selected_type = 0;
-    char buffer[size];
-
-    #if !defined(__OPTIMIZE__)
-      bool debug_instantiated = false;
-    #endif
-
-    template <typename T>
-    constexpr inline uint get_selector_for_type() {
-      return get_selector_for_type_impl<T, Types...>();
-    }
-
-    template <typename T, typename First, typename... Rest>
-    constexpr inline uint get_selector_for_type_impl() {
-      if constexpr (std::is_same_v<T, First>) {
-        return sizeof...(Types) - sizeof...(Rest);
-      } 
-      
-      else if constexpr (sizeof...(Rest) > 0) {
-        return get_selector_for_type_impl<T, Rest...>();
+        
+        else {
+          static_assert(!std::is_same_v<T, T>, "Unsupported type");
+        }
       }
-      
-      else {
-        static_assert(!std::is_same_v<T, T>, "Unsupported type");
+  
+      template <typename First, typename... Rest>
+      void destroy_object_impl() {
+        if (selected_type == sizeof...(Types) - sizeof...(Rest)) {
+          reinterpret_cast<First*>(buffer)->~First();
+        }
+        
+        else if constexpr (sizeof...(Rest) > 0) {
+          destroy_object_impl<Rest...>();
+        }
+        
+        else {
+          throw std::runtime_error("Possibly corrupt selected_type");
+        }
       }
-    }
-
-    template <typename First, typename... Rest>
-    void destroy_object_impl() {
-      if (selected_type == sizeof...(Types) - sizeof...(Rest)) {
-        reinterpret_cast<First*>(buffer)->~First();
-      }
-      
-      else if constexpr (sizeof...(Rest) > 0) {
-        destroy_object_impl<Rest...>();
-      }
-      
-      else {
-        throw std::runtime_error("Possibly corrupt selected_type");
-      }
-    }
-};
+  };
+}
