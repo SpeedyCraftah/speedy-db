615d366fa824ccf9b6dbe0afa6cbacc4e474844f
Jacek <jacek@speedydev.org>
2025-10-05 03:28:05 +0100

    move type functions to header

--- c/src/storage/structures/types.h
+++ c/src/storage/structures/types.h
@@ -3,25 +3,41 @@
 #include <cstddef>
 #include <string_view>
 #include "stdint.h"
+#include "../table-basic.h"
 
-enum ColumnType : uint32_t {
-    Integer,
-    Float32,
-    Long64,
-    Byte,
-    String
-};
+// Converts a column type into a readable string.
+constexpr std::string_view column_type_to_string(ColumnType type) {
+    if (type == ColumnType::Integer) return "integer";
+    else if (type == ColumnType::String) return "string";
+    else if (type == ColumnType::Byte) return "byte";
+    else if (type == ColumnType::Float32) return "float";
+    else if (type == ColumnType::Long64) return "long";
+    __builtin_unreachable();
+}
 
-constexpr std::string_view column_type_to_string(ColumnType type);
-constexpr ColumnType string_to_column_type(std::string_view type);
-constexpr size_t column_type_alignof(ColumnType type);
-constexpr size_t column_type_sizeof(ColumnType type);
+constexpr ColumnType string_to_column_type(std::string_view type) {
+    if (type == "integer") return ColumnType::Integer;
+    else if (type == "string") return ColumnType::String;
+    else if (type == "byte") return ColumnType::Byte;
+    else if (type == "float") return ColumnType::Float32;
+    else if (type == "long") return ColumnType::Long64;
+    else return (ColumnType)-1;
+}
 
-union NumericColumnData {
-    size_t unsigned64_raw = 0;
-    uint32_t unsigned32_raw;
-    long long64;
-    uint8_t byte;
-    int int32;
-    float float32;
-};
+constexpr size_t column_type_alignof(ColumnType type) {
+    if (type == ColumnType::Integer) return alignof(int);
+    else if (type == ColumnType::String) return alignof(size_t);
+    else if (type == ColumnType::Byte) return alignof(uint8_t);
+    else if (type == ColumnType::Float32) return alignof(float);
+    else if (type == ColumnType::Long64) return alignof(long);
+    __builtin_unreachable();
+}
+
+constexpr size_t column_type_sizeof(ColumnType type) {
+    if (type == ColumnType::Integer) return sizeof(int);
+    else if (type == ColumnType::String) return sizeof(HashedColumnData);
+    else if (type == ColumnType::Byte) return sizeof(uint8_t);
+    else if (type == ColumnType::Float32) return sizeof(float);
+    else if (type == ColumnType::Long64) return sizeof(long);
+    __builtin_unreachable();
+}
